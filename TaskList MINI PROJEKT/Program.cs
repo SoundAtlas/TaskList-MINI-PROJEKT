fbee5d3cd7073c335824094e968da4465a9abd4f
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-05-07 12:42:18 +0200

    Add project files.

---
+++ TaskList MINI PROJEKT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskList_MINI_PROJEKT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
